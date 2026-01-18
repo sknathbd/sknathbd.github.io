117af71e0e53216e7e3a8b9acbeec54559b525a3
sknathbd <srinath.cox@gmail.com>
2026-01-18 22:23:23 +0600

    adding flutter map

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3178935917"
+    serviceWorkerVersion: "3490833757"
   }
 });
